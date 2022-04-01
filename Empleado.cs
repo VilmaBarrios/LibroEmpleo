bc4def89811570e53f43c6e73eb0a3303796726b
VilmaBarrios <VilmaBarrios@users.noreply.github.com>
2022-03-31 20:15:48 -0600

    Agregar archivos de proyecto.

---
+++ Empleado.cs
@@ -0,0 +1,45 @@
+﻿public abstract class Empleado
+{
+    private string primerNombre;
+    private string apellidoPaterno;
+    private string numeroSeguroSocial;
+
+
+    public Empleado(string nombre, string apellido, string nss)
+    {
+        primerNombre = nombre;
+        apellidoPaterno = apellido;
+        numeroSeguroSocial = nss;
+    }
+
+
+    public string PrimerNombre
+    {
+        get 
+        {
+            return primerNombre;
+        }
+    }
+
+
+    public string ApellidoPaterno
+    {
+        get
+        {
+            return apellidoPaterno;
+        }
+    } 
+    public string NumeroSeguroSocialcial del empleado
+    {
+        get
+        {
+            return numeroSeguroSocial;
+        } 
+    } 
+    public override string ToString()
+    {
+        return string.Format("{0} {1}\nnúmero de seguro social: {2}",
+        PrimerNombre, ApellidoPaterno, NumeroSeguroSocial);
+    } 
+    public abstract decimal Ingresos(); 
+} 
---
+++ EmpleadoAsalariado.cs
@@ -0,0 +1,30 @@
+﻿ public class EmpleadoAsalariado : Empleado
+ {
+ private decimal salarioSemanal;
+
+ public EmpleadoAsalariado(string nombre, string apellido, string nss,
+ decimal salario) : base(nombre, apellido, nss)
+ {
+ SalarioSemanal = salario; 
+ } 
+ public decimal SalarioSemanal
+ {
+   get
+ {
+         return salarioSemanal;
+         } 
+     set
+ {
+         salarioSemanal = ((value >= 0) ? value : 0); 
+         } 
+     } 
+ public override decimal Ingresos()
+ {
+     return SalarioSemanal;
+     } 
+ public override string ToString()
+ {
+     return string.Format("empleado asalariado: {0}\n{1}: {2:C}",
+     base.ToString(), "salario semanal", SalarioSemanal);
+     } 
+ } 
